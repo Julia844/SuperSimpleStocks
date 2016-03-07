4e6c61ba0a8e9ef0563bfd8788fefc2caf5185f6
tgorka <tgorka@luxia.fr>
2016-03-07 23:53:44 +0100

    finish gbce #7

--- supersimplestocks/spark.py
+++ supersimplestocks/spark.py
@@ -8,6 +8,50 @@
 #
 from pyspark import SparkContext, SparkConf
 
+
+def map_stock_price(trade):
+    '''
+    Spark Map function for calculating stock price
+    :param trade: for calculation
+    :return: calculated value
+    '''
+    quantity = trade['quantity'] - trade['open_quantity']
+    return trade['price'] * float(quantity), quantity
+
+
+def reduce_stock_price(price_quantity1, price_quantity2):
+    '''
+    Spark Reduce function for calculating stock price
+    :param price_quantity1: first object to combine
+    :param price_quantity2: second object to combine
+    :return: calculated value
+    '''
+    return (price_quantity1[0] + price_quantity2[0],
+            price_quantity1[1] + price_quantity2[1])
+
+
+def postfix_stock_price(price, quantity):
+    '''
+    Postfix function to calculate results from Spark reduce function
+    for calculating stock price
+    :param price: to calculate
+    :param quantity: to calculate
+    :return: calculated value
+    '''
+    return price / float(quantity) if quantity > 0 else None
+
+
+def postfix_gbce(count, p):
+    '''
+    Postfix function to calculate results from Spark reduce function
+    for calculating GBCE
+    :param p: to calculate
+    :param count: to calculate
+    :return: calculated value
+    '''
+    return p ** (1.0 / float(count))
+
+
 class Spark():
     # private spark context variable
     __spart_context = None
@@ -52,24 +96,18 @@ class Spark():
         :param trades: to analyze
         :return: calculated value
         '''
-        def mapFun(trade):
-            quantity = trade['quantity'] - trade['open_quantity']
-            return trade['price'] * float(quantity), quantity
-
-        filteredRdd = self.spark_context.parallelize(trades). \
+        filtered_rdd = self.spark_context.parallelize(trades). \
             filter(lambda trade: trade['symbol'] == symbol and
                                  trade['datetime'] >= begin_datetime and
                                  trade['open_quantity'] < trade['quantity'])
-        if filteredRdd.isEmpty():
+        if filtered_rdd.isEmpty():
             return None
 
-        price, quantity = filteredRdd. \
-            map(mapFun). \
-            reduce(lambda price_quantity1, price_quantity2:
-                    (price_quantity1[0] + price_quantity2[0],
-                     price_quantity1[1] + price_quantity2[1]))
+        price, quantity = filtered_rdd. \
+            map(map_stock_price). \
+            reduce(reduce_stock_price)
 
-        return price / float(quantity) if quantity > 0 else None
+        return postfix_stock_price(price, quantity)
 
 
     def last_dividend(self, symbol, dividents):
@@ -79,23 +117,33 @@ class Spark():
         :param dividents: to analyze
         :return: last dividend from the list or throw a ValueError if not exists
         '''
-        filteredRdd  =  self.spark_context.parallelize(dividents). \
+        filtered_rdd  =  self.spark_context.parallelize(dividents). \
             filter(lambda divident: divident['symbol'] == symbol)
 
-        return None if filteredRdd.isEmpty() else filteredRdd.first()
+        return None if filtered_rdd.isEmpty() else filtered_rdd.first()
 
 
     def gbce(self, begin_datetime, trades):
-        filteredRdd  = self.spark_context.parallelize(trades). \
+        '''
+        Calculate GBCE based on the realized trades since the given time
+        for all stocks.
+        Algorithm: geometric mean of all stock prices: RootN(Sp1*Sp2*...SpN)
+        :param begin_datetime: begin date since to analyze
+        :param trades: to analyze
+        :return: calculated value
+        '''
+        filtered_rdd  = self.spark_context.parallelize(trades). \
             filter(lambda trade: trade['datetime'] >= begin_datetime and
                                  trade['open_quantity'] < trade['quantity'])
-        if filteredRdd.isEmpty():
+        if filtered_rdd.isEmpty():
             return None
 
-        count, p = filteredRdd. \
-            map(lambda trade: (1, trade['price'])). \
+        count, p = filtered_rdd. \
+            map(lambda trade: (trade['symbol'], map_stock_price(trade))). \
+            reduceByKey(reduce_stock_price). \
+            map(lambda symbol: (1, postfix_stock_price(*symbol[1]))). \
             reduce(lambda count_p1, count_p2: (count_p1[0] + count_p2[0],
                                                count_p1[1] * count_p2[1]))
 
-        # count is always > 0 because filteredRdd is not empty
-        return p ** (1.0 / float(count))
+        # count is always > 0 because filtered_rdd is not empty
+        return postfix_gbce(count, p)
