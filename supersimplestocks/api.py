b4aa1f43b8651b7e5dc77068278dba71d6d8abee
tgorka <tgorka@luxia.fr>
2016-03-07 21:58:02 +0100

    validation of the input values #7

--- supersimplestocks/api.py
+++ supersimplestocks/api.py
@@ -13,8 +13,10 @@ from supersimplestocks import tasks
 
 def load_dividend_data(csv_path):
     '''
-    Load dividend data from csv file and registed each value to the celery task.
+    Load dividend data from csv file and registered each value
+    to the celery task.
     :param csv_path: path to the file
+    :raise ValueError: if value or last is < 0 from any row in csv file
     '''
     with open(os.path.abspath(csv_path), 'r') as csvfile:
         for index,stock in enumerate(csv.reader(csvfile, delimiter=';')):
@@ -28,8 +30,9 @@ def record_dividend_data(symbol, type, last, fixed, value):
     :param symbol: of the stock
     :param type: Common, Preferred
     :param last: dividend value
-    :param fixed: percentage value in format 0.01
+    :param fixed: percentage value in format 0.01, or 1% otherwise set as None.
     :param value: par value
+    :raise ValueError: if value or last is < 0
     '''
     # if it's like 2% or 0.02
     if fixed is not None and fixed.endswith('%'):
@@ -39,8 +42,15 @@ def record_dividend_data(symbol, type, last, fixed, value):
     else:
         fixed = None
 
-    tasks.record_dividend_data.delay(
-            symbol, type, float(last), fixed, int(value))
+    value = int(value)
+    last = float(last)
+
+    if value < 0:
+        raise ValueError('Par value should be >= 0, not %d' % value)
+    if last < 0:
+        raise ValueError('Last dividend should be >= 0, not %f' % last)
+
+    tasks.record_dividend_data.delay(symbol, type, last, fixed, value)
 
 
 def record_trade(symbol, type, quantity, price):
@@ -53,15 +63,24 @@ def record_trade(symbol, type, quantity, price):
     :param type: SELL, BUY
     :param quantity: to trade
     :param price: of the trade
+    :raise ValueError: if price or quantity is < 0
     '''
-    tasks.record_trade.delay(symbol, type, int(quantity), float(price))
+    price = float(price)
+    quantity = int(quantity)
+
+    if price < 0.0:
+        raise ValueError('Price should be >= 0, not %f' % price)
+    if quantity < 0:
+        raise ValueError('Quantity should be >= 0, not %d' % quantity)
+
+    tasks.record_trade.delay(symbol, type, quantity, price)
 
 
 def stock_price(symbol):
     '''
     Calculate stock price for given stock based on trades from last 15 min.
     :param symbol: of the stock
-    :return: value
+    :return: value or None if can't calculate value
     '''
     return tasks.stock_price.delay(symbol).get(timeout=300)
 
@@ -71,7 +90,7 @@ def dividend_yield(symbol):
     Calculate dividend yield for given stock.
     The initial data about the dividends are static taken from csv file.
     :param symbol: of the stock
-    :return: value or throw a ValueError if symbol not registered
+    :return: value or None if symbol not registered
             in last dividents.
     '''
     return tasks.dividend_yield.delay(symbol).get(timeout=300)
@@ -82,7 +101,7 @@ def p_e_ratio(symbol):
     Calculate P/E Ratio for given stock.
     The initial data about the dividends are static taken from csv file.
     :param symbol: of the stock
-    :return: value
+    :return: value or None if can't calculate value
     '''
     return tasks.p_e_ratio.delay(symbol).get(timeout=300)
 
@@ -91,7 +110,7 @@ def gbce():
     '''
     Calculate the GBCE All Share Index using the geometric mean of prices
     for all stocks.
-    :return: value
+    :return: value or None if can't calculate value
     '''
     return tasks.gbce.delay().get(timeout=300)
 
